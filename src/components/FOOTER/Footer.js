7b0374295d69afaafbcd34f1839617284bc05a7d
thutra23 <tathutra12a@gmail.com>
2021-12-05 00:38:15 -0800

    feat: make navbar fixed, added footer

---
+++ src/components/FOOTER/Footer.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+const Footer = props => {
+    return ( 
+        <div className="footer">
+            <p>©HikeIt 2021 All rights reserved</p>
+        </div>
+     );
+}
+ 
+export default Footer;
